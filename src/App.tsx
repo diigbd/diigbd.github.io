569eff55dfe6dd05f000ed602c0d0e8a2c22b659
Ian Le <ianlllym@gmail.com>
2026-03-04 00:15:36 -0500

    Fix site deployment

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/DIIG-Master-Website">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
